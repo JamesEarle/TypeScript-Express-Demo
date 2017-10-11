3e87f111d9cbeef542e6769b6b0526de9684fa29
James Earle <jaearle@microsoft.com>
2017-10-11 11:12:52 -0700

    color-demo ready, need to modify demo that uses OOP

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 // app.ts
+
 import * as express from 'express';
 import * as body from 'body-parser';
 import * as path from 'path';
-import {Student} from './models';
 
 let app = express();
 
@@ -11,22 +11,22 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.use(body.json());
-app.use(body.urlencoded({ extended : false }));
+app.use(body.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req: any, res: any) {
+app.get('/', function (req: any, res: any) {
     res.render('index');
 });
 
-app.post('/submit', function(req:any, res:any) {
+app.post('/submit', function (req: any, res: any) {
     res.render('index', {
         color: req.body.color
     });
 });
 
 // Express 4.X Middleware
-app.use(function(req: any, res: any, next: any) {});
-app.use(function(err: any, req: any, res: any, next: any) {});
+app.use(function (req: any, res: any, next: any) { });
+app.use(function (err: any, req: any, res: any, next: any) { });
 
 app.listen(app.get('port'));
 
